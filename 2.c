abf6c95d6fb9d57e41fbbd6f8d849582d31487c1
Piya <piya.rat@ku.th>
2021-03-29 17:39:07 +0700

    2

--- 2.c
+++ 2.c
@@ -1,4 +1,29 @@
 #include<stdio.h>
     void main(){
-
+        int n,i,scores[10000]={0},min=10000,max=0,choose,maxs=0,check=0;
+        scanf("%d",&n);
+        for( i=0 ; i<n ; i++ ){
+            scanf("%d",&choose);
+            if(max<choose){
+                max=choose;
+            }
+            if(min>choose){
+                min=choose;
+            }
+            scores[choose]++;
+        }
+        for( i=min ; i<=max ; i++ ){
+            if(scores[i]>maxs){
+                maxs=i;
+                check++;
+            }
+        }
+        if(check>1){
+            for( i=min ; i<=max ; i++ ){
+                if(scores[i]==maxs){
+                    printf("%d ",i);
+                }
+            }
+        }
+        printf("%d",maxs);
     }
