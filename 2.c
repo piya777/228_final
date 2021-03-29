89c8bfa4f424f88756abbb2e9065597ab4ab58b7
Piya <piya.rat@ku.th>
2021-03-29 19:29:21 +0700

    5

--- 2.c
+++ 2.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
     void main(){
-        int n,i,scores[10001]={0},min=10000,max=0,choose,maxs=0,check=0;
+        int x,n,i,scores[10001]={0},min=10000,max=0,choose,maxs=0,check=0;
         scanf("%d",&n);
-        for( i=0 ; i<n ; i++ ){
-            scanf("%d",&choose);
-            if(max<choose){
-                max=choose;
+        if(n>=2&&n<=1000){
+            for( i=0 ; i<n ; i++ ){
+                scanf("%d",&choose);
+                if(choose<1||choose>10000){
+                    x++;
+                    break;
+                }
+                if(max<choose){
+                    max=choose;
+                }
+                if(min>choose){
+                    min=choose;
+                }
+                scores[choose]++;
             }
-            if(min>choose){
-                min=choose;
+            if(x==0){
+                for( i=min ; i<=max ; i++ ){
+                    if(scores[i]>=maxs){
+                        maxs=i;
+                    }
+                }
+                for( i=min ; i<=max ; i++ ){
+                    if(scores[maxs]==scores[i]){
+                        printf("%d ",i);
+                    }
+                }
             }
-            scores[choose]++;
         }
-        for( i=min ; i<=max ; i++ ){
-            if(scores[i]>=maxs){
-                maxs=i;
-            }
-        }
-        for( i=min ; i<=max ; i++ ){
-            if(scores[maxs]==scores[i]){
-                printf("%d ",i);
-            }
-        }
-        //printf("%d",maxs);
     }
